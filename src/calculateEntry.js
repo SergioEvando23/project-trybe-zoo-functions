40fceff024ab418493066642138fa0f7071e03f1
SergioEvando23 <sergioevandocosta@gmail.com>
2022-01-07 16:06:19 -0300

    função calculateEntry completa

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,25 @@
-const data = require('../data/zoo_data');
+const { prices } = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const persons = { adult: 0, child: 0, senior: 0 };
+
+  if (!Array.isArray(entrants)) return persons;
+
+  entrants.forEach((person) => {
+    if (person.age < 18) {
+      persons.child += 1;
+    } else if (person.age < 50) {
+      persons.adult += 1;
+    } else {
+      persons.senior += 1;
+    }
+  });
+  return persons;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  const { adult, child, senior } = countEntrants(entrants);
+  return adult * prices.adult + child * prices.child + senior * prices.senior;
 }
 
 module.exports = { calculateEntry, countEntrants };
