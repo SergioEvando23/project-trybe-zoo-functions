7cd8a8d8f8a8219d796d2ef8b153b44db04db22a
SergioEvando23 <sergioevandocosta@gmail.com>
2022-01-10 15:13:59 -0300

    função getSchedule.js

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,28 @@
 const data = require('../data/zoo_data');
 
-function getAnimalMap(options) {
-  // seu código aqui
+function filterGenre(genre) {
+  const animals = [];
+  data.species.forEach((specie) => {
+    const test = specie.residents.some((animal) => animal.sex === genre.sex);
+    if (test) {
+      animals.push(specie.name);
+    }
+  });
+  return animals;
 }
-
+function speciesByLocation() {
+  const animalsLocation = {
+    NE: [],
+    NW: [],
+    SE: [],
+    SW: [],
+  };
+  data.species.forEach((animal) => {
+    animalsLocation[animal.location].push(animal.name);
+  });
+  return animalsLocation;
+}
+function getAnimalMap(options) {
+  return speciesByLocation();
+}
 module.exports = getAnimalMap;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,46 @@
-const data = require('../data/zoo_data');
+const { hours, species } = require('../data/zoo_data');
+
+function getAvailabilityByDay(day) {
+  return species.reduce((acc, specie) => {
+    if (specie.availability.includes(day)) {
+      acc.push(specie.name);
+    }
+    return acc;
+  }, []);
+}
+
+function createSchedule(daysTarget) {
+  return daysTarget.reduce((acc, day) => {
+    if (day === 'Monday') {
+      acc[day] = { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' };
+      return acc;
+    }
+    acc[day] = {
+      officeHour: `Open from ${hours[day].open}am until ${hours[day].close}pm`,
+      exhibition: getAvailabilityByDay(day),
+    };
+    return acc;
+  }, {});
+}
 
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  const speciesNames = species.map((specie) => specie.name);
+  const days = Object.keys(hours);
+
+  if (speciesNames.includes(scheduleTarget)) {
+    return species.find((animal) => animal.name === scheduleTarget).availability;
+  }
+
+  if (!scheduleTarget || !days.includes(scheduleTarget)) {
+    return createSchedule(days);
+  }
+  if (scheduleTarget) {
+    return createSchedule([scheduleTarget]);
+  }
 }
 
 module.exports = getSchedule;
+getSchedule();
+
+
+//Função completa com a ajuda do aluno Laecio Silva turma xp - b
