9677c4e5a530edf167ca833e9381cf381e1d33dc
SergioEvando23 <sergioevandocosta@gmail.com>
2022-01-10 15:28:18 -0300

    função getSchedule.js - corrigindo o  lint

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,28 +1,6 @@
 const data = require('../data/zoo_data');
 
-function filterGenre(genre) {
-  const animals = [];
-  data.species.forEach((specie) => {
-    const test = specie.residents.some((animal) => animal.sex === genre.sex);
-    if (test) {
-      animals.push(specie.name);
-    }
-  });
-  return animals;
-}
-function speciesByLocation() {
-  const animalsLocation = {
-    NE: [],
-    NW: [],
-    SE: [],
-    SW: [],
-  };
-  data.species.forEach((animal) => {
-    animalsLocation[animal.location].push(animal.name);
-  });
-  return animalsLocation;
-}
 function getAnimalMap(options) {
-  return speciesByLocation();
+  // seu codigo aqui
 }
 module.exports = getAnimalMap;
